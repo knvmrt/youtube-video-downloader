a72026e4a5b50a3e883ed9cf5d65bbb2c10291f4
Murad KnV <99596011+knvmrt@users.noreply.github.com>
2023-02-16 10:06:31 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ try:
     yt = YouTube(link)
     print(f"Downloading '{yt.title}'...")
 
-    video = yt.streams.filter(progressive=True, 
-    file_extension='mp4').order_by('resolution').desc().first()
+    video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
     video.download()
 
     print("Video downloaded!")
